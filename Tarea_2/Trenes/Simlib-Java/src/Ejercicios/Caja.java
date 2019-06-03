ff1277551c13f05a75e12987fd761ef9e99f9f5c
Vale <vbernalb@unal.edu.co>
2019-06-03 15:10:20 -0500

    cajas

---
+++ Tarea_2/Trenes/Simlib-Java/src/Ejercicios/Caja.java
@@ -0,0 +1,16 @@
+package Ejercicios;
+
+public class Caja {
+	public String tipo;
+	public int peso;
+	public float llegada;
+	
+	public Caja(String tipo,int peso, float llegada) {
+		this.tipo = tipo;
+		this.peso = peso;
+		this.llegada = llegada;
+	}
+	
+	
+
+}
